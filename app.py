f4f51236b7248df535d7d16ae8032d325ba48795
Ankit Gupta <Ankit.Gupta4@rb.com>
2024-02-27 20:14:55 +0530

    4th commit

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+from io import BytesIO
+from PIL import Image
+import requests
 import streamlit as st
 
 st.write("Welcome to Streamlit")
-st.write("Select different pages to proceed")
+
+img_url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIkFuZS8SuA68sIurQObxCReuu6PJRhwvzmw&usqp=CAU"
+response = requests.get(img_url)
+
+image = Image.open(BytesIO(response.content))
+
+st.image(image)
